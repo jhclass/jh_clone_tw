da9c82f6f864db0e1a32ec5e6d69394f3ca58976
Jinhyoung Lee <jhcode0@naver.com>
2022-09-28 18:02:06 +0900

    로그아웃 후 홈으로 돌아가기

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,17 +5,25 @@ import {
     Routes,
     Route,
   } from "react-router-dom";
+import Navigation from "./Navigation";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
 
 
  const AppRouter = ({isLoggedIn})=> {
     
     return(
        <BrowserRouter>
+            {isLoggedIn && <Navigation/>}
         <Routes>
                 {isLoggedIn?
-                (<Route path="/" element={<Home />}></Route>)
+                (
+               <>
+                  <Route path="/" element={<Home />}></Route>
+                  <Route path="/profile" element={<Profile />}></Route>
+               </>
+                )
                 :           
                 (<Route path="/" element={<Auth />}></Route>)}
         </Routes>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,2 +1,21 @@
 import React from "react";
-export default ()=> <span>Profile</span>;
+import { getAuth, signOut } from "firebase/auth";
+export default ()=> {
+    const onLogOutClick=()=> {
+        const auth = getAuth();
+        signOut(auth).then(() => {
+        // Sign-out successful.
+        console.log('로그아웃됨');
+        const history = useHistory();
+        history.push("/");
+
+        }).catch((error) => {
+        // An error happened. 
+        });
+    }
+    return(
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+}
